6c58a30162adf5e6577136e03dd3773e7892b6a4
babalolajnr <babalolajnr@gmail.com>
2021-04-12 14:02:31 +0100

    moved round robin class functionality to weighted round robin class

--- src/WeightedRoundRobin.java
+++ src/WeightedRoundRobin.java
@@ -2,17 +2,41 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
-public class WeightedRoundRobin extends RoundRobin {
+public class WeightedRoundRobin extends LoadBalancer {
 
-    public WeightedRoundRobin(Map<String, Integer> ipMap) {
-        super(ipMap.keySet().stream().map(ip -> {
+    private int counter = 0;
+    private final ReentrantLock lock;
+
+    public WeightedRoundRobin(Map<String, Integer> nodeMap) {
+
+        super(nodeMap.keySet().stream().map(ip -> {
             List<String> tempList = new LinkedList<>();
-            for (int i = 0; i < ipMap.get(ip); i++) {
+            for (int i = 0; i < nodeMap.get(ip); i++) {
                 tempList.add(ip);
             }
             return tempList;
         }).flatMap(Collection::stream).collect(Collectors.toList()));
+
+        lock = new ReentrantLock();
+    }
+
+    // Get the next available node the counter points to and increment the counter
+    // by one
+    @Override
+    public String getNode() {
+        lock.lock();
+        try {
+            String ip = nodeList.get(counter);
+            counter += 1;
+            if (counter == nodeList.size()) {
+                counter = 0;
+            }
+            return ip;
+        } finally {
+            lock.unlock();
+        }
     }
 }
