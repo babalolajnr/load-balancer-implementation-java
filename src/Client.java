6a26ba1be99657ca14a1ccb4a0d59afe732fa179
babalolajnr <babalolajnr@gmail.com>
2021-04-12 14:00:43 +0100

    refactored some variable names

--- src/Client.java
+++ src/Client.java
@@ -3,10 +3,9 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 /**
- * Progarm for the load balancer system
- * this program uses a weighted round robin implementation
- * of a load balancer to distribute 16 requests to the available nodes
- * by examining the weights of each node
+ * Progarm for the load balancer system this program uses a weighted round robin
+ * implementation of a load balancer to distribute 16 requests to the available
+ * nodes by examining the weights of each node
  * 
  */
 public class Client {
@@ -16,14 +15,15 @@ public class Client {
         int NUM_OF_REQUESTS = 16;
         Client client = new Client();
 
-        Map<String, Integer> ipPoolWeighted = new HashMap<>();
-        ipPoolWeighted.put("192.168.0.1", 7);
-        ipPoolWeighted.put("192.168.0.2", 5);
-        ipPoolWeighted.put("192.168.0.3", 3);
-        ipPoolWeighted.put("192.168.0.4", 1);
+        // A hashMap of 4 separate nodes is created
+        Map<String, Integer> nodePool = new HashMap<>();
+        nodePool.put("192.168.0.1", 7);
+        nodePool.put("192.168.0.2", 5);
+        nodePool.put("192.168.0.3", 3);
+        nodePool.put("192.168.0.4", 1);
 
         client.printNextTurn("Weighted-Round-Robin");
-        LoadBalancer weightedRoundRobin = new WeightedRoundRobin(ipPoolWeighted);
+        LoadBalancer weightedRoundRobin = new WeightedRoundRobin(nodePool);
         client.simulateConcurrentClientRequest(weightedRoundRobin, NUM_OF_REQUESTS);
 
         System.out.println("Main exits");
@@ -32,7 +32,7 @@ public class Client {
 
     private void simulateConcurrentClientRequest(LoadBalancer loadBalancer, int numOfCalls) {
 
-        IntStream.range(0, numOfCalls).parallel().forEach(i -> System.out.println("IP: " + loadBalancer.getIp()
+        IntStream.range(0, numOfCalls).parallel().forEach(i -> System.out.println("IP: " + loadBalancer.getNode()
                 + " --- Request from Client: " + i + " --- [Thread: " + Thread.currentThread().getName() + "]"));
     }
 
